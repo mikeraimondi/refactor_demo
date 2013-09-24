73bb2e208a1c4c53af7796d8c7b99944662c4838
Michael Raimondi <zeno@pobox.com>
2013-09-23 21:50:05 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+RefactorDemo::Application.config.secret_key_base = '4722648a515bb5119d0566ab2df250df66791aa9ef3490eca3666cf4c1e679f83c728d1f05b7b88386f932088afcf822f327fa0f249211dd508abb91e1f2b911'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+RefactorDemo::Application.config.session_store :cookie_store, key: '_refactor_demo_session'
